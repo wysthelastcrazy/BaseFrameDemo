2bc185f3e72bd86a46d7d3daaee854f45051cb54
wysthelastcrazy <wysthelastcrazy@163.com>
2018-03-13 16:46:40 +0800

    CollapsingToolbar简单实例添加

---
+++ app/src/main/java/com/beta/MoneyballMaster/activity/CollapsingToolbarActivity.java
@@ -0,0 +1,19 @@
+package com.beta.MoneyballMaster.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.beta.MoneyballMaster.R;
+
+/**
+ * Created by yas on 2018/3/13.
+ */
+
+public class CollapsingToolbarActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapsing_toolbar);
+    }
+}
